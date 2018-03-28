f4f2e4e0a10e0e1f3d734d76f1c4013b6a146087
LukeTomalty <37132676+LukeTomalty@users.noreply.github.com>
2018-03-27 18:36:24 -0700

    added
    added new files about/schedule

---
+++ schedule.php
@@ -0,0 +1,23 @@
+<?php include 'templates/htmlTop.php';?>
+<!-- include if specific styles are needed (aka remove both comment lines)-->
+<link rel="stylesheet" href="styles/schedule.css">
+<?php include 'templates/header.php';?>
+<!-- html body code -edit body below -->
+<select>
+    <option value="">-Select Day-</option>
+    <option value="day1">Day 1</option>
+    <option value="day2">Day 2</option>
+    <option value="day3">Day 3</option>
+    <option value="day4">Day 4</option>
+    <option value="day5">Day 5</option>
+    <option value="day6">Day 6</option>
+    <option value="day7">Day 7</option>
+    <option value="day8">Day 8</option>
+    <option value="day9">Day 9</option>
+    <option value="day10">Day 10</option>
+    <option value="day11">Day 11</option>
+    <option value="day12">Day 12</option>
+</select> 
+
+<!-- html body code -edit body above -->
+<?php include 'templates/footer.php';?>
--- templates/footer.php
+++ templates/footer.php
@@ -1,21 +1,21 @@
 </main>
-	<footer>
-		<div class="row">
-			<div class="one-third column">
-				<h2>one</h2>
-			</div>
-			<div class="one-third column">
-				<h2>two</h2>
-				<p></p>
-				<p></p>
-			</div>
-			<div class="one-third column">
-				<h2>three</h2>
-				<p></p>
-				<p></p>
-			</div>
+<footer>
+	<div class="row">
+		<div class="one-third column">
+			<h2>one</h2>
 		</div>
-	</footer>
+		<div class="one-third column">
+			<h2>two</h2>
+			<p></p>
+			<p></p>
+		</div>
+		<div class="one-third column">
+			<h2>three</h2>
+			<p></p>
+			<p></p>
+		</div>
+	</div>
+</footer>
 </div>
 
 <script src="./scripts/menu.js"></script>
